58ca3de285d2d31349dc0b31699efe4d0781fa18
Ksenia <silencio_pr@protonmail.com>
2020-05-03 20:23:02 +0300

    Add test for promo pages

---
+++ test_promo_page_stepik.py
@@ -0,0 +1,23 @@
+import pytest
+from .pages.product_page import ProductPage
+import time
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.should_be_book_title_on_page("Coders at Work")  # проверяем что на странице есть название книги
+    page.add_to_basket()  # добавляем товар в корзину
+    page.solve_quiz_and_get_code()  # считаем результат
+    time.sleep(60)
+    page.should_be_message_with_book_title()    # проверяем что в сообщении с названием содержится название книги
+    page.should_be_message_with_book_price()    # провреяем что в сообщении с ценой содержится правильная цена
